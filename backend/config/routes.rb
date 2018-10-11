a6e839a77e5cf0b7b7a376991063faa7f72b7375
RebGov <becci.govert@gmail.com>
2018-10-11 18:00:25 -0500

    notes showing. User can login

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
-      resources :notes, only: [:create, :read, :update, :delete]
+      resources :notes, only: [:create, :read, :update, :delete, :search]
       resources :photos, only: [:create, :read, :update, :delete]
 
 
