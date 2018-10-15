07fdc77e1837e340375c13ee3d2e8034474c49e3
RebGov <becci.govert@gmail.com>
2018-10-15 13:40:40 -0500

    fixed authorization on frontend typo not have access to user.notes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       resources :notes, only: [:create, :read, :update, :delete, :search]
+      get'/notes', to: 'notes#index'
       resources :photos, only: [:create, :read, :update, :delete]
 
 
---
+++ frontend/src/components/AllNotes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class AllNotes extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state={
+  //
+  //   }
+  // }
+  render() {
+    const style = { border: "1px solid brown", padding: "1rem", margin: "1rem" };
+    return(
+      <div style={style}>Hello All Notes</div>
+    )
+  }
+}
